222246f26bb3d91e57a48d848e6ac252b11a9722
FonderElite <71190959+FonderElite@users.noreply.github.com>
2020-12-27 19:46:21 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 sudo apt-get update
 sudo apt install python3
 sudo apt install python3-pip
+pip3 install -r requirements.txt
 echo "Done."
 echo"
  ███████████                ███████████                         █████                    ██████████ ████   ███   █████            
